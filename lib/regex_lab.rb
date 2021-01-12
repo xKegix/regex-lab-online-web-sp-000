f178a234e02cf515c1ab363b91ed09af7a7d94d4
Keginold Philip <keginoldp@hotmail.com>
2021-01-12 16:45:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
